7a6590693442e47670d2b71f4b9decc0fbbd7f17
LeviTas2003 <levitas2003@gmail.com>
2023-06-20 14:42:32 +0200

    Prepared for merge

--- app/config/Config.php
+++ app/config/Config.php
@@ -9,7 +9,7 @@ define('DB_NAME', 'foodbank');
 define('APPROOT', dirname(dirname(__FILE__)));
 
 // de url van de website
-define('URLROOT', 'http://www.foodbank-maaskantje.local');
+define('URLROOT',  'http://www.foodbank-maaskantje.local');
 
 // naam van de website
 define('SITENAME',  'voedselbank-maaskantje');

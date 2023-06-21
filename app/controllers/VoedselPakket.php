b4e177d3ccc2b857a15c4fb1a5cf8cd3718f47df
LeviTas2003 <levitas2003@gmail.com>
2023-06-21 12:36:23 +0200

    Made some changes. Vertegenwoordiger shows full name and has tussenvoegsel check

--- app/controllers/VoedselPakket.php
+++ app/controllers/VoedselPakket.php
@@ -10,7 +10,7 @@ class VoedselPakket extends controller
     public function index()
     {
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            $records = $this->model->getVoedselPakketten();
+            $records = $this->model->getVoedselPakkettenByEetwens();
         } else {
             $records = $this->model->getVoedselPakketten();
         }
@@ -21,20 +21,25 @@ class VoedselPakket extends controller
         if ($records == null) {
             $rows .= '<h2>Er staan nog geen leveranciers in de database.</h2> ';
         } else {
-            // foreach ($records as $value) {
-            //     // Bouwt de tabel inhoud
-            //     $rows .= "
-            //     <tr>
-            //         <td>$value->companyname</td>
-            //         <td>$value->address</td>
-            //         <td>$value->contactperson</td>
-            //         <td>$value->email</td>
-            //         <td>$value->phonenumber</td>
-            //         <td>$value->nextdelivery</td>
-            //         <td><a href='../supplier/updateSupplier/$value->id' class='btn-outlined-green'>Edit</a></td>
-            //         <td><a href='../supplier/deleteSupplier/$value->id' class='btn-outlined-red'>Delete</a></td>
-            //     </tr>";
-            // }
+            foreach ($records as $value) {
+                // Bouwt de tabel inhoud
+                $fullname = '';
+                if ($value->tussenvoegsel == null) {
+                    $fullname = "$value->voornaam $value->achternaam";
+                } else {
+                    $fullname = "$value->voornaam $value->tussenvoegsel $value->achternaam";
+                }
+                $rows .= "
+                <tr>
+                    <td>$value->naam</td>
+                    <td>$value->omschrijving</td>
+                    <td>$value->volwassenen</td>
+                    <td>$value->kinderen</td>
+                    <td>$value->babys</td>
+                    <td>$fullname</td>
+                    <td><a href='../supplier/updateSupplier/$value->id' class='btn-outlined-green'>Edit</a></td>
+                </tr>";
+            }
         }
 
         // array voor alle data om mee te sturen naar de view
--- app/views/Voedselpakket/index.php
+++ app/views/Voedselpakket/index.php
@@ -1,2 +1,24 @@
-<?= var_dump($data['naam']); ?>
 <H3>Homepage voedselbank maaskantje</H3>
+<?= require(APPROOT . '/views/includes/Navbar.php'); ?>
+<div class="grid">
+    <div class="container fd-c col-12-lg ai-fs">
+        <table class="table table-striped">
+            <thead>
+                <th>Naam</th>
+                <th>Omschrijving</th>
+                <th>Volwassenen</th>
+                <th>Kinderen</th>
+                <th>Baby's</th>
+                <th>Vertegenwoordiger</th>
+                <th>VoedselPakket Details</th>
+            </thead>
+            <tbody>
+                <?= $data['rows']; ?>
+            </tbody>
+            <!-- <a class="btn-grey" href="../supplier/createSupplier">Add supplier</a> -->
+        </table>
+    </div>
+</div>
+
+<?= var_dump($data['naam']); ?>
+<?php require(APPROOT . '/views/includes/Footer.php'); ?>
